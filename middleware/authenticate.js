472482fb0486d75c15bcea9103bb38506d2679a9
Antoni1830 <jemmotte08@gmail.com>
2024-10-12 23:18:08 -0400

    tareaaaa

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,7 +1,10 @@
-const isAuthenticated= (req,res,next)=>{
-    if(req.session.user ===undefined){
-        return res.status(401).json("qlq el mioooo")
+const isAuthenticated = (req, res, next) => {
+    if (req.session && req.session.user) {
+      next();
+    } else {
+      return res.status(401).json({ message: "Authentication required" });
+      // #swagger.responses[401] = { description: 'Authentication required' }
     }
-    next();
-};
-module.exports ={ isAuthenticated}
+  };
+  
+  module.exports = isAuthenticated;
--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,15 @@ const router =express.Router();
 const usersController=require('../contrtollers/users');
 const{validateCreate}=require('../Validator/users');
 
-const{ isAuthenticated}=require ("../middleware/authenticate")
+const{isAuthenticated}=require ("../middleware/authenticate");
 
 router.get('/', usersController.getAll);
 router.get('/:id',usersController.getSingle);
 router.post('/',validateCreate, usersController.createUser);
-router.put('/:id',usersController.updateUser);
+router.put('/:id', usersController.updateUser);
 router.delete('/:id', usersController.deleteUser);
+
+
 
 
 module.exports=router;
