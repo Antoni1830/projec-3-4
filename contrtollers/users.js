6079200e7667d7930da2b6aa028d479322467b3e
Antoni1830 <jemmotte08@gmail.com>
2024-10-12 22:48:58 -0400

    ok ok

--- contrtollers/users.js
+++ contrtollers/users.js
@@ -76,10 +76,11 @@ const updateUser= async(req,res)=>async(req,res) =>{
     //#swagger.tags=['Games']
     const userId=new ObjectId(req.params.id);
     const user={
-    username:req.body.username,
-    email:req.body.email,
-    name:req.body.name,  
-    ipaddress:req.body.ipaddress,
+      firstName:req.body.firstName,
+      lastName:req.body.lastName,
+      email:req.body.email,  
+      favoriteColor:req.body.favoriteColor,
+      birthday:req.body.birthday
 };
 const response= await mongodb.getDatabase().db().collection('Games').replaceOne({id: userId}, user);
      if (response.modifiedCount>0){
